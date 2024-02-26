58604fda740ae17640ed374c0b66c7fb4a9a88e8
eehanseul <gkstmf616@gmail.com>
2024-02-26 11:36:25 +0900

    tmp do not /search

--- routes/drinks.js
+++ routes/drinks.js
@@ -5,22 +5,39 @@ const Brewer = require("../model/Brewer");
 
 /* GET : drinks listing. */
 router.get("/", function (req, res, next) {
-  Drink.find().populate('brewerId').then((data) => {
-    res.status(200).json(data);
-  }).catch((err) => {
-    res.status(404).json({ error: 'Do not get drink list' });
-  });
+  Drink.find()
+    .populate("brewerId")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(404).json({ result: "false" });
+    });
 });
 
 /* GET : drink detail */
 router.get("/:drinkId", function (req, res, next) {
   const drinkId = req.params.drinkId;
 
-  Drink.findById(drinkId).populate("brewerId").then((data) => {
+  Drink.findById(drinkId)
+    .populate("brewerId")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(404).json({ result: false });
+    });
+});
+
+/* GET : drink search */
+router.get("/search", function (req, res, next) {
+  
+  Drink.find().populate("brewerId").then((data) => {
     res.status(200).json(data);
   }).catch((err) => {
-    res.status(404).json({ error: 'Do not get drink list' });
+    res.status(404).json({ error: err.message });
   });
+
 });
 
 module.exports = router;
